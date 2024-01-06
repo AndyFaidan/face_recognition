7c70b1d7f232c07c2cd46baa6ae817d82c742c24
andy sofyan guspriyanto <129332680+AndyFaidan@users.noreply.github.com>
2024-01-06 11:03:31 +0700

    Update Face_Recognition_main.py

--- Face_Recognition_main.py
+++ Face_Recognition_main.py
@@ -1,28 +1,28 @@
 import streamlit as st
 import cv2
 import face_recognition as frg
-import yaml 
+import yaml
 from utils import recognize, build_dataset
 
 # Path: code\app.py
 st.set_page_config(layout="wide")
 
-# Konfigurasi
+# Configuration
 cfg = yaml.load(open('config.yaml','r'), Loader=yaml.FullLoader)
 PICTURE_PROMPT = cfg['INFO']['PICTURE_PROMPT']
 WEBCAM_PROMPT = cfg['INFO']['WEBCAM_PROMPT']
 
 st.sidebar.title("Pengaturan")
 
-# Buat menu bar
+# Create menu bar
 menu = ["Foto", "Webcam"]
 pilihan = st.sidebar.selectbox("Jenis Input", menu)
 
-# Geser untuk menyesuaikan toleransi
+# Slide to adjust tolerance
 TOLERANCE = st.sidebar.slider("Toleransi", 0.0, 1.0, 0.5, 0.01)
 st.sidebar.info("Toleransi adalah ambang batas untuk pengenalan wajah. Semakin rendah toleransinya, semakin ketat pengenalan wajahnya. Semakin tinggi toleransinya, semakin longgar pengenalan wajahnya.")
 
-# Bagian Informasi 
+# Information Section
 st.sidebar.title("Informasi Mahasiswa")
 nama_container = st.sidebar.empty()
 nim_container = st.sidebar.empty()
@@ -35,28 +35,28 @@ if pilihan == "Foto":
     foto_diunggah = st.file_uploader("Unggah", type=['jpg', 'png', 'jpeg'], accept_multiple_files=True)
 
     if foto_diunggah:
-        # Baca gambar yang diunggah dengan face_recognition
+        # Read the uploaded image with face_recognition
         for gambar in foto_diunggah:
             gambar = frg.load_image_file(gambar)
-            gambar, nama, nim = recognize(gambar, TOLERANCE) 
+            gambar, nama, nim = recognize(gambar, TOLERANCE)
             nama_container.info(f"Nama: {nama}")
             nim_container.success(f"NIM: {nim}")
             st.image(gambar)
-    else: 
+    else:
         st.info("Mohon unggah sebuah gambar")
-    
+
 elif pilihan == "Webcam":
     st.title("Aplikasi Pengenalan Wajah")
     st.write(WEBCAM_PROMPT)
 
-    # Pengaturan Kamera
-    kamera = cv2.VideoCapture(1)
+    # Camera Setup
+    kamera = cv2.VideoCapture(0)
     kamera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
     kamera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
     FRAME_WINDOW = st.image([])
 
     while True:
-        # Ambil frame dari kamera
+        # Capture frame from the camera
         berhasil, bingkai = kamera.read()
 
         if not berhasil:
@@ -64,17 +64,16 @@ elif pilihan == "Webcam":
             st.info("Harap matikan aplikasi lain yang menggunakan kamera dan restart aplikasi")
             st.stop()
 
-        # Proses frame untuk pengenalan wajah
+        # Process the frame for face recognition
         gambar, nama, nim = recognize(bingkai, TOLERANCE)
         gambar = cv2.cvtColor(gambar, cv2.COLOR_BGR2RGB)
 
-        # Tampilkan nama dan NIM orang tersebut
+        # Display the name and NIM of the person
         nama_container.info(f"Nama: {nama}")
         nim_container.success(f"NIM: {nim}")
 
-        # Tampilkan frame dengan Streamlit
+        # Display the frame with Streamlit
         FRAME_WINDOW.image(gambar, channels="BGR")
-
 
 with st.sidebar.form(key='my_form'):
     st.title("Bagian Pengembang")
