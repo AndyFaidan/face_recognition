ace64978446c5fe0fbc594c8cbc7313a50b1931e
andy sofyan guspriyanto <129332680+AndyFaidan@users.noreply.github.com>
2024-01-06 11:03:14 +0700

    Update Database.py

--- pages/Database.py
+++ pages/Database.py
@@ -3,27 +3,22 @@ import pickle
 import yaml 
 import pandas as pd 
 
-# Mengambil konfigurasi dari file YAML
 cfg = yaml.load(open("config.yaml", "r"), Loader=yaml.FullLoader)
 PKL_PATH = cfg['PATH']["PKL_PATH"]
-
-# Mengatur tata letak halaman Streamlit
 st.set_page_config(layout="wide")
 
-# Memuat database
+# Load database 
 with open(PKL_PATH, 'rb') as file:
     database = pickle.load(file)
 
-# Membuat kolom-kolom untuk menampilkan informasi
-Indeks, NIM, Nama, Gambar = st.columns([0.5, 0.5, 3, 3])
+Index, Nim, Name, Image = st.columns([0.5, 0.5, 3, 3])  # Ganti 'Id' menjadi 'Nim'
 
-# Menampilkan informasi setiap wajah dalam database
 for idx, person in database.items():
-    with Indeks:
+    with Index:
         st.write(idx)
-    with NIM: 
-        st.write(person['nim'])
-    with Nama:     
+    with Nim:  # Ganti 'Id' menjadi 'Nim'
+        st.write(person['nim'])  # Ganti 'id' menjadi 'nim'
+    with Name:     
         st.write(person['name'])
-    with Gambar:     
+    with Image:     
         st.image(person['image'], width=200)
