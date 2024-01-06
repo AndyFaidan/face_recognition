4c99e1ee6f9a70de757484403dfe68fd83d59254
andy sofyan guspriyanto <129332680+AndyFaidan@users.noreply.github.com>
2024-01-06 11:03:48 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,123 +6,127 @@ import numpy as np
 import yaml
 from collections import defaultdict
 
-# Menggunakan DefaultDict untuk database
-database = defaultdict(dict)
-
-# Mengambil path dari file konfigurasi langsung
+information = defaultdict(dict)
 cfg = yaml.load(open('config.yaml', 'r'), Loader=yaml.FullLoader)
+DATASET_DIR = cfg['PATH']['DATASET_DIR']
 PKL_PATH = cfg['PATH']['PKL_PATH']
-DATASET_DIR = cfg['PATH']['DATASET_DIR']  # Menambahkan definisi DATASET_DIR dari konfigurasi
 
 def get_database():
     try:
         with open(PKL_PATH, 'rb') as f:
             database = pkl.load(f)
-        return database
     except FileNotFoundError:
-        return {}
+        # Buat database kosong jika file tidak ditemukan
+        database = defaultdict(dict)
+        with open(PKL_PATH, 'wb') as f:
+            pkl.dump(database, f)
+    return database
 
-def recognize(image, TOLERANCE):
+def recognize(image, TOLERANCE): 
     database = get_database()
-    known_encoding = [database[nim]['encoding'] for nim in database.keys()]
-    name, nim = 'Tidak Dikenal', 'Tidak Dikenal'
-
+    known_encoding = [database[idx]['encoding'] for idx in database.keys()] 
+    name = 'Unknown'
+    nim = 'Unknown'
     face_locations = frg.face_locations(image)
     face_encodings = frg.face_encodings(image, face_locations)
-
     for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
         matches = frg.compare_faces(known_encoding, face_encoding, tolerance=TOLERANCE)
-        distances = frg.face_distance(known_encoding, face_encoding)
-
+        distance = frg.face_distance(known_encoding, face_encoding)
+        name = 'Unknown'
+        nim = 'Unknown'
         if True in matches:
             match_index = matches.index(True)
-            name, nim = database[match_index]['name'], database[match_index]['nim']
-            distance = round(distances[match_index], 2)
+            name = database[match_index]['name']
+            nim = database[match_index]['nim']
+            distance = round(distance[match_index], 2)
             cv2.putText(image, str(distance), (left, top - 30), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
-
         cv2.rectangle(image, (left, top), (right, bottom), (0, 255, 0), 2)
         cv2.putText(image, name, (left, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
-
     return image, name, nim
 
-def is_face_exists(image):
+def isFaceExists(image): 
     face_location = frg.face_locations(image)
-    return len(face_location) != 0
+    if len(face_location) == 0:
+        return False
+    return True
 
-def submit_new(name, nim, image, old_idx=None):
+def submitNew(name, nim, image, old_idx=None):
     database = get_database()
-
-    # Baca gambar
-    if isinstance(image, np.ndarray):
+    # Read image 
+    if type(image) != np.ndarray:
         image = cv2.imdecode(np.frombuffer(image.read(), np.uint8), 1)
 
-    is_face_in_pic = is_face_exists(image)
-
-    if not is_face_in_pic:
+    isFaceInPic = isFaceExists(image)
+    if not isFaceInPic:
         return -1
-
-    # Encode gambar
+    # Encode image
     encoding = frg.face_encodings(image)[0]
-
-    # Tambahkan ke database
-    # Periksa apakah nim sudah ada
-    existing_nim = [database[n]['nim'] for n in database.keys()]
-
-    # Mode pembaruan
-    if old_idx is not None:
+    # Append to database
+    # Check if nim already exists
+    existing_nim = [database[idx]['nim'] for idx in database.keys()]
+    # Update mode 
+    if old_idx is not None: 
         new_idx = old_idx
-    # Mode penambahan
-    else:
+    # Add mode
+    else: 
         if nim in existing_nim:
             return 0
         new_idx = len(database)
-
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    database[new_idx] = {'image': image, 'nim': nim, 'name': name, 'encoding': encoding}
-
+    database[new_idx] = {
+        'image': image,
+        'nim': nim, 
+        'name': name,
+        'encoding': encoding
+    }
     with open(PKL_PATH, 'wb') as f:
         pkl.dump(database, f)
-
     return True
 
-def get_info_from_nim(nim):
-    database = get_database()
-    for idx, person in database.items():
-        if person['nim'] == nim:
-            return person['name'], person['image'], idx
+def get_info_from_nim(nim): 
+    database = get_database() 
+    for idx, person in database.items(): 
+        if person['nim'] == nim: 
+            name = person['name']
+            image = person['image']
+            return name, image, idx       
     return None, None, None
 
-def delete_one(nim):
+def deleteOne(nim):
     database = get_database()
     nim = str(nim)
-
-    for key, person in list(database.items()):  # Menambahkan fungsi list() agar iterasi tidak terpengaruh saat menghapus item
+    for key, person in database.items():
         if person['nim'] == nim:
             del database[key]
             break
-
     with open(PKL_PATH, 'wb') as f:
         pkl.dump(database, f)
-
     return True
 
 def build_dataset():
+    # Pastikan direktori 'dataset' sudah ada
+    if not os.path.exists(DATASET_DIR):
+        os.makedirs(DATASET_DIR)
+
+    counter = 0
     for image in os.listdir(DATASET_DIR):
         image_path = os.path.join(DATASET_DIR, image)
         image_name = image.split('.')[0]
         parsed_name = image_name.split('_')
         person_nim = parsed_name[0]
         person_name = ' '.join(parsed_name[1:])
-
         if not image_path.endswith('.jpg'):
             continue
-
         image = frg.load_image_file(image_path)
-        database[len(database)] = {'image': image, 'nim': person_nim, 'name': person_name, 'encoding': frg.face_encodings(image)[0]}
+        information[counter]['image'] = image 
+        information[counter]['nim'] = person_nim
+        information[counter]['name'] = person_name
+        information[counter]['encoding'] = frg.face_encodings(image)[0]
+        counter += 1
 
-    with open(PKL_PATH, 'wb') as f:
-        pkl.dump(database, f)
+    with open(os.path.join(DATASET_DIR, 'database.pkl'), 'wb') as f:
+        pkl.dump(information, f)
 
-if __name__ == "__main__":
-    # Gunakan untuk menguji fungsionalitas utils (bila dijalankan sebagai skrip langsung)
-    pass
+if __name__ == "__main__": 
+    build_dataset()
+    deleteOne(1)
